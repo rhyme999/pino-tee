f195ba6ada792a77af3fea23c14aaf4c1fbb1e21
Andrew <a.sharov@eastwood.com.ru>
2019-10-18 15:34:51 +0300

    Make strict level check

--- tee.js
+++ tee.js
@@ -1,83 +1,85 @@
 #! /usr/bin/env node
-'use strict'
+"use strict";
 
-const split = require('split2')
-const cloneable = require('cloneable-readable')
-const pump = require('pump')
-const Parse = require('fast-json-parse')
-const minimist = require('minimist')
-const pino = require('pino')
-const fs = require('fs')
+const split = require("split2");
+const cloneable = require("cloneable-readable");
+const pump = require("pump");
+const Parse = require("fast-json-parse");
+const minimist = require("minimist");
+const pino = require("pino");
+const fs = require("fs");
 
-function tee (origin) {
-  const clone = cloneable(origin)
+function tee(origin) {
+  const clone = cloneable(origin);
 
-  clone.tee = function (dest, filter) {
-    filter = filter || alwaysTrue
-    pump(this.clone(), buildFilter(filter), dest)
-    return dest
-  }
+  clone.tee = function(dest, filter) {
+    filter = filter || alwaysTrue;
+    pump(this.clone(), buildFilter(filter), dest);
+    return dest;
+  };
 
-  return clone
+  return clone;
 }
 
-function buildFilter (filter) {
-  if (typeof filter === 'string') {
-    const num = pino.levels.values[filter]
+function buildFilter(filter) {
+  if (typeof filter === "string") {
+    const num = pino.levels.values[filter];
 
-    if (typeof num === 'number' && isFinite(num)) {
-      filter = function (v) { return v.level >= num }
+    if (typeof num === "number" && isFinite(num)) {
+      filter = function(v) {
+        return v.level == num;
+      };
     } else {
-      throw new Error('no such level')
+      throw new Error("no such level");
     }
   }
 
-  return split(function (line) {
-    const res = new Parse(line)
+  return split(function(line) {
+    const res = new Parse(line);
     if (res.value && filter(res.value)) {
-      return JSON.stringify(res.value) + '\n'
+      return JSON.stringify(res.value) + "\n";
     } else {
-      return undefined
+      return undefined;
     }
-  })
+  });
 }
 
-function alwaysTrue () {
-  return true
+function alwaysTrue() {
+  return true;
 }
 
-module.exports = tee
+module.exports = tee;
 
 if (require.main === module) {
-  start()
+  start();
 }
 
-function start () {
-  const args = minimist(process.argv.slice(2))
-  const pairs = []
-  var i
+function start() {
+  const args = minimist(process.argv.slice(2));
+  const pairs = [];
+  var i;
 
   if (args._.length % 2) {
-    console.error('pino-tee requires an even amount of args')
-    console.error('Usage: pino-tee [filter dest]..')
-    process.exit(1)
+    console.error("pino-tee requires an even amount of args");
+    console.error("Usage: pino-tee [filter dest]..");
+    process.exit(1);
   }
 
   for (i = 0; i < args._.length; i += 2) {
-    let dest = args._[i + 1]
-    if (dest === ':2') {
-      dest = process.stderr
+    let dest = args._[i + 1];
+    if (dest === ":2") {
+      dest = process.stderr;
     } else {
-      dest = fs.createWriteStream(dest, {flags: 'a'})
+      dest = fs.createWriteStream(dest, { flags: "a" });
     }
 
     pairs.push({
       filter: args._[i],
       dest
-    })
+    });
   }
 
-  const instance = tee(process.stdin)
-  pairs.forEach(pair => instance.tee(pair.dest, pair.filter))
-  instance.pipe(process.stdout)
+  const instance = tee(process.stdin);
+  pairs.forEach(pair => instance.tee(pair.dest, pair.filter));
+  instance.pipe(process.stdout);
 }
